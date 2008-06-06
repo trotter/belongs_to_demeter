2b1f79b6d2c26aec56a19241a7d9c212bb8228f8
Trotter Cashion <cashion@gmail.com>
2008-06-05 23:10:24 -0400

    Add proper response to query

--- lib/belongs_to_demeter.rb
+++ lib/belongs_to_demeter.rb
@@ -1,2 +1,35 @@
 module BelongsToDemeter
+  def self.included(base)
+    base.send(:extend, ClassMethods)
+    base.send(:include, InstanceMethods)
+  end
+
+  module ClassMethods
+    def belongs_to(*args)
+      belongs_to_demeter << args.first.to_sym
+    end
+
+    def belongs_to_demeter
+      @__belongs_to_demeter ||= []
+    end
+  end
+
+  module InstanceMethods
+    def respond_to?(meth)
+      for pre in self.class.belongs_to_demeter
+        return true if meth.to_s =~ /^#{pre}_/
+      end
+      super
+    end
+
+    def method_missing(meth, *args, &block)
+      for pre in self.class.belongs_to_demeter
+        if meth.to_s =~ /^#{pre}_/
+          obj = self.send(pre)
+          return obj ? obj.send($') : nil
+        end
+      end
+      super
+    end
+  end
 end
--- test/belongs_to_demeter_test.rb
+++ test/belongs_to_demeter_test.rb
@@ -1,13 +1,33 @@
 require 'test/unit'
+require File.expand_path(File.dirname(__FILE__) + "/../../../../config/environment")
 require File.dirname(__FILE__) + '/../lib/belongs_to_demeter'
 
 class BelongsToDemeterTest < Test::Unit::TestCase
-  def setup
-    c = Class.new
-    c.send(:include, BelongsToDemeter)
+  class User
+    def login
+      'login'
+    end
   end
 
-  def test_runs
-    assert 'yay'
+  def setup
+    @c = Class.new(ActiveRecord::Base)
+    class << @c
+      def columns; []; end
+    end
+    @c.send(:include, BelongsToDemeter)
+  end
+
+  def test_responds_to_belongs_to_attribute
+    @c.belongs_to :user
+    assert @c.new.respond_to?(:user_login)
+  end
+
+  def test_calls_belongs_to_attribute
+    @c.send(:define_method, :user) { User.new }
+    @c.belongs_to :user
+    assert_equal User.new.login, @c.new.user_login
+
+    @c.send(:define_method, :user) { nil }
+    assert_equal nil, @c.new.user_login
   end
 end
